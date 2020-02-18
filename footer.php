483a482433d46feddc887236136c7e923007935d
Sean Tucker <sean_tucker366@hotmail.com>
2020-02-18 12:58:34 -0500

    added privacy and disclaimer

--- footer.php
+++ footer.php
@@ -1,9 +1,20 @@
 </div>
 </div>
 </body>
-<footer>
+<footer style="background:#343a40; min-height: 50pt; ">
 
-<h2>This is the footer</h2>
+  <ul class="nav justify-content-center">
+    <li class="nav-item" style="padding: 10px">
+      <a class="nav-link" href="contactUs.php" style="color: white;">Contact Us</a>
+    </li>
+    <li class="nav-item" style="padding: 10px">
+      <a class="nav-link" href="disclaimer.php" style="color: white;">Disclaimer</a>
+    </li>
+    <li class="nav-item" style="padding: 10px">
+      <a class="nav-link" href="privacy.php" style="color: white;">Privacy Policy</a>
+    </li>
+  </ul>
+
 
 
 
---
+++ privacy.php
@@ -0,0 +1,40 @@
+<?php
+  include 'header.php';
+?>
+
+<h2 style="color: white; padding: 5% 5% 0% 5%; text-align: center " ><b>Privacy Policy</b></h2>
+<div class:"content" style="color: white; padding: 0% 6% 3% 6%;">
+
+<p>
+This privacy Policy discloses the privacy practices for Rollingbenches.ca. This privacy notice applies solely to information collected
+ by this website. It will notify you of What personal data we collect and why we collect it.
+</p>
+    <ul style="list-style-type:none; padding: 4% 0% 1% 0%; ">
+        <li> <h3>Contact forms </h3>
+            <ul style="list-style-type:none;">
+                <li style="margin: 30px 0;">In ordinary use of this website, a user may choose to complete the contact form on our Contact Us page. Any information as listed
+                     in the current version of the form that is submitted <b>is not stored on our servers </b>. The information is sent securely via email using our 
+                     mail forwarding provider (mailgun.com) to one of our owner/employees using a gmail apps account in order to provide sales and/or order assistance.</li>
+            </ul>
+        </li>
+        <li> <h3>Google Analytics </h3>
+            <ul style="list-style-type:none;">
+                <li style="margin: 30px 0;">We use Google Analytics to inform ourselves with respect to location and demographic data for our traffic, behaviour flow & acquisition.</li>
+            </ul>
+        </li>
+        <li> <h3> Who we share your data with </h3>
+            <ul style="list-style-type:none;">
+                <li style="margin: 30px 0;"><p>Per our contact form we only share information internally to assist potential buyers and customers.</p></li>
+            </ul>
+        </li>
+    </ul>
+    <p> <br> Email Us with Questions at: policy@rollingbenches.ca</p>
+
+    
+
+</div>
+
+
+<?php
+  include 'footer.php';
+  ?>
